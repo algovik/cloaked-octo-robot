11776cab7b2b065b35af18033ea4adb808a3b055
Andreas Algovik <algovik@gmail.com>
2014-01-28 13:30:32 +0100

    updated

--- client.js
+++ client.js
@@ -1,6 +1,31 @@
-displayView = function(){
-   // the code required to display a view
-};
-document.onload = function(){
-    //code that is executed as the page is loaded.
-};
+function loadView(){
+	document.getElementById("welcomeView").innerHTML = document.getElementById("welcomeBody").innerHTML;
+}
+
+function validateLogin(){
+	var x = document.forms["login"]["username"].value;
+	if (x==null||x=="")
+	{
+		document.getElementById("username").style.border="1px dotted red";
+		// document.login.username.focus();
+		return false;
+	}
+
+	x = document.forms["login"]["password"].value;
+	if (x==null||x=="")
+	{
+		document.getElementById("password").style.border="1px dotted red";
+		// document.login.password.focus();
+		return false;
+	}
+
+	return true;
+}
+
+
+// displayView = function(){
+//    // the code required to display a view.
+// };
+// document.onload = function(){
+//     //code that is executed as the page is loaded.
+// };
