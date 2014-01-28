5723bd73eb016eb85d067e59026f9706e325af51
Andreas Algovik <algovik@gmail.com>
2014-01-28 14:57:13 +0100

    changed js

--- client.js
+++ client.js
@@ -3,23 +3,61 @@ function loadView(){
 }
 
 function validateLogin(){
+	var bool=true;
 	var x = document.forms["login"]["username"].value;
 	if (x==null||x=="")
 	{
-		document.getElementById("username").style.border="1px dotted red";
-		// document.login.username.focus();
-		return false;
+		document.getElementById("username").className ="invalidEntry";
+		bool=false;
+	}
+	else{
+		document.getElementById("username").className="standardBorder";
 	}
 
 	x = document.forms["login"]["password"].value;
 	if (x==null||x=="")
 	{
-		document.getElementById("password").style.border="1px dotted red";
-		// document.login.password.focus();
+		document.getElementById("password").className ="invalidEntry";
+		bool=false;
+	}
+	else{
+		document.getElementById("password").className="standardBorder";
+	}
+	return bool;
+}
+
+function validateSignup(){
+	var x = document.forms["signup"]["firstName"].value;
+	if(x==null||x=="")
+	{
+		document.getElementById("firstName").style.border="1px dotted red";
 		return false;
 	}
 
-	return true;
+	x = document.forms["signup"]["familyName"].value;
+	if(x==null||x=="")
+	{
+		document.getElementById("familyName").style.border="1px dotted red";
+		return false;
+	}
+
+	x = document.forms["signup"]["city"].value;
+	if(x==null||x=="")
+	{
+		document.getElementById("city").style.border="1px dotted red";
+		return false;
+	}
+
+	x = document.forms["signup"]["country"].value;
+	if(x==null||x=="")
+	{
+		document.getElementById("country").style.border="1px dotted red";
+		return false;
+	}
+
+
+
+
 }
 
 
