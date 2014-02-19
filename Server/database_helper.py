cc6fbd671781c6e33e82a2b3ecc146f6a09cc4c7
Andreas Algovik <algovik@gmail.com>
2014-02-19 21:42:22 +0100

    added token and hash functions

--- Server/database_helper.py
+++ Server/database_helper.py
@@ -2,6 +2,11 @@
 #and control the database and shall contain some SQL scripts.
 #This file will be used by the server to access the database.
 
+def connect_db():
+	rv = sqlite3.connect(app.config['DATABASE'])
+	rv.row_factory = sqlite3.Row
+	return rv
+
 def get_db():
 	if not hasattr(g, 'sqlite_db'):
 		g.sqlite_db = connect_db()
@@ -10,7 +15,9 @@ def get_db():
 def close_db():
 	if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
+    return ''
 
-def get_password(email):
+def get_password(user_email):
 	db = get_db()
-	db.execute('')
+	cur = db.execute('SELECT password FROM users WHERE email=' + user_email + ';')
+	return cur.fetchone()
--- Server/server.py
+++ Server/server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from random import choice
 import database_helper
+import hashlib
 
 app = Flask(__name__)
 app.debug = True
@@ -12,11 +13,10 @@ app.debug = True
 def sign_in(email, password):
     try:
         if verify_password(email, password):
-            #create randomized token
             token = ''
             letters = ['a','b','c','d','e','f','g','h','i','k','l','m','n','o','p','q','r','s','t','u','v','w','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0']
-                for i in range(36):
-                    token+= choice(letters)    
+                for i in xrange(36):
+                    token += choice(letters)
 
             add_logged_in_user(email, token) #Should we keep a log of logged in users in db?
             return {'success':True,'message':'Successfully signed in.','data':token}
@@ -29,45 +29,61 @@ def sign_in(email, password):
 @app.route('/signup')
 def sign_up(email, password, firstname, familyname, gender, city, country):
     #Call to appropirate function(s) in database_helper
+    return ''
 
 @app.route('/signout')
 def sign_out(token):
+    #Call to appropriate function(s) in database_helper
+    return ''
 
 
 @app.route('/changepassword')
 def change_password(token, old_password, new_password):
     #Call to appropirate function(s) in database_helper
+    return ''
 
 @app.route('/getuserdatabytoken')
 def get_user_data_by_token(token):
     #Call to appropirate function(s) in database_helper
+    return ''
 
 @app.route('/getuserdatabyemail')
 def get_user_data_by_email(token, email):
     #Call to appropirate function(s) in database_helper
+    return ''
 
 @app.route('/getusermessagesbytoken')
 def get_user_messages_by_token(token):
     #Call to appropirate function(s) in database_helper
+    return ''
 
 @app.route('/getusermessagesbyemail')
 def get_user_messages_by_email(token, email):
     #Call to appropirate function(s) in database_helper
+    return ''
 
 @app.route('/postmessage')
 def post_message(token, message, email):
     #Call to appropirate function(s) in database_helper
+    return ''
+
+@app.route('/hash')
+def hash(password):
+    return ''
 
 
 #Local functions
 def verify_password(email, password):
-    hash = ''
-    #create hash of password
-
+    hash = hash_pwd(password)
     if hash == get_password(email):
         return True
     else:
         return False
+
+def hash_pwd(password):
+    m = hashlib.sha224()
+    m.update(password)
+    return m.hexdigest()
 
 
 @app.teardown_appcontext
@@ -75,5 +91,5 @@ def teardown_app(exception):
     #Close after app context is teared down.
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     app.run()
