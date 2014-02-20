999bd7aff3e092916c6eac1e1a7896f6e7f0512b
Andreas Algovik <algovik@gmail.com>
2014-02-20 22:48:30 +0100

    fixed some server_helper functions, but it seems not all work in all contexts.

--- Server/database_helper.py
+++ Server/database_helper.py
@@ -16,35 +16,53 @@ def close_db():
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
+
+#Works
 def verify_email(email):
     db = get_db()
-    cur = db.execute("SELECT Email FROM Users WHERE Email=" + email)
-    close_db(); # We probobly should close db connections roojjttt?
+    cur = db.execute("SELECT Email FROM Users WHERE Email='" + email + "'")
     if cur.fetchone() is None:
+        close_db()
         return False
     else:
+        close_db()
         return True
 
+#Doesn't work
 #Takes in a dictionary containing the attributes: email, password, firstname, familyname, gender, city, country
 def insert_new_user(user):
     db = get_db()
-    cur = db.execute("INSERT INTO Users (Email, Password, Firstname, Familyname, Gender, City, Country) VALUES (?,?,?,?,?,?,?)", (user["email"], user["password"], user["firstname"], user["familyname"], user["gender"], user["city"], user["country"]))
-    cur.commit()
+    db.execute("INSERT INTO Users (Email, Password, Firstname, Familyname, Gender, City, Country) VALUES (?,?,?,?,?,?,?)", (user["email"], user["password"], user["firstname"], user["familyname"], user["gender"], user["city"], user["country"]))
+    db.commit()
+    close_db()
 
+#Works for test function add_user
 def add_logged_in_user(email, token):
     db = get_db()
-    cur = db.execute("INSERT INTO LoggedInUsers (Email, Token) VALUES (?,?)", (email,token))
-    cur.commit()
+    db.execute("INSERT INTO LoggedInUsers (Email, Token) VALUES (?,?)", (email,token))
+    db.commit()
+    close_db()
 
+#Works for test function remove_user, but not for sign_out
+def remove_logged_in_user(token):
+    db = get_db()
+    db.execute("DELETE FROM LoggedInUsers WHERE Token='" + token + "'")
+    db.commit()
+    close_db()
+
+#Doesn't work
 def get_password(email):
     db = get_db()
-    cur = db.execute("SELECT Password FROM Users WHERE Email=" + email)
-    return cur.fetchone
+    cur = db.execute("SELECT Password FROM Users WHERE Email='" + email + "'")
+    return cur.fetchone()
 
+#Works for test function read_user
 def check_if_logged_in(token):
     db = get_db()
-    cur = db.execute("SELECT Token FROM LoggedInUsers WHERE Token=" + token)
+    cur = db.execute("SELECT * FROM LoggedInUsers WHERE Token='" + token + "'")
     if cur.fetchone() is None:
+        close_db()
         return False
     else:
+        close_db()
         return True
--- Server/server.py
+++ Server/server.py
@@ -25,17 +25,20 @@ def sign_in(email, password):
 
 #Useful to know about locals(): the dictionary does not neccessary contain the parameters in the expected order.
 #Parameter identifiers should be used when trying to retrieve a specific value.
-@app.route('/signup')
+@app.route('/signup/<email>/<password>/<firstname>/<familyname>/<gender>/<city>/<country>')
 def sign_up(email, password, firstname, familyname, gender, city, country):
     if database_helper.verify_email(email)==False:
         if validate_signup(locals()):
             database_helper.insert_new_user(locals())
+            return {'success':True, 'message':'Successfully created a new user.'}        
+        else:
+            return {'success':False, 'message':'Formdata not complete.'}
+    else:
+        return {'success':False, 'message':'User already exists.'}
 
-    return ''
-
-@app.route('/signout')
+@app.route('/signout/<token>')
 def sign_out(token):
-    if check_if_logged_in(token):
+    if database_helper.check_if_logged_in(token):
         database_helper.remove_logged_in_user(token)
         return {'success':True,'message':'Successfully signed out'}
     else:
@@ -72,22 +75,34 @@ def post_message(token, message, email):
     #Call to appropirate function(s) in database_helper
     return ''
 
-
+#Test functions
 @app.route('/verify/<email>')
 def verify_email(email):
     if database_helper.verify_email(email):
-        return 'Success'
+        return 'User exists.\n'
     else:
-        return 'Failure'
+        return 'User does not exist.\n'
 
 @app.route('/adduser/<email>/<token>')
 def add_user(email, token):
     database_helper.add_logged_in_user(email, token)
+    return 'Success'
+
+@app.route('/removeuser/<token>')
+def remove_user(token):
+    database_helper.remove_logged_in_user(token)
+    return 'Success'
 
 @app.route('/readuser/<token>')
 def read_user(token):
-    database_helper.check_if_logged_in(token)
+    if database_helper.check_if_logged_in(token):
+        return 'User is logged in.\n'
+    else:
+        return 'User is not logged in.\n'
 
+@app.route('/getpassword/<email>')
+def get_password(email):
+    return database_helper.get_password(email)
 
 
 #Local functions
@@ -104,7 +119,7 @@ def hash_pwd(password):
     return m.hexdigest()
 
 def validate_signup(formDictionary):
-    for val in dictionary.values():
+    for val in formDictionary.values():
         if not_none(val)!=True:
             return False
 
