058afe1487d233c9894fe976621d2fec9036479a
Andreas Algovik <algovik@gmail.com>
2014-02-20 14:25:10 +0100

    breakthrough in server access

--- Server/server.py
+++ Server/server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import g
 from random import choice
 import database_helper
 import hashlib
@@ -11,12 +12,12 @@ app.debug = True
 #HTTP functions
 @app.route('/signin')
 def sign_in(email, password):
-    if verify_email(email) and verify_password(email, password):
+    if database_helper.verify_email(email) and verify_password(email, password):
         token = ''
         letters = ['a','b','c','d','e','f','g','h','i','k','l','m','n','o','p','q','r','s','t','u','v','w','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0']
-            for i in xrange(36):
-                token += choice(letters)
-        add_logged_in_user(email, token)
+        for i in xrange(36):
+            token += choice(letters)
+        database_helper.add_logged_in_user(email, token)
         return {'success':True,'message':'Successfully signed in.','data':token}
     else:
         return {'success':False,'message':'Wrong username or password.'}
@@ -29,7 +30,7 @@ def sign_up(email, password, firstname, familyname, gender, city, country):
 @app.route('/signout')
 def sign_out(token):
     if check_if_logged_in(token):
-        remove_logged_in_user(token)
+        database_helper.remove_logged_in_user(token)
         return {'success':True,'message':'Successfully signed out'}
     else:
         return {'success':False,'message':'You are not signed in.'}
@@ -66,10 +67,27 @@ def post_message(token, message, email):
     return ''
 
 
+@app.route('/verify/<email>')
+def verify_email(email):
+    if database_helper.verify_email(email):
+        return 'Success'
+    else:
+        return 'Failure'
+
+@app.route('/adduser/<email>/<token>')
+def add_user(email, token):
+    database_helper.add_logged_in_user(email, token)
+
+@app.route('/readuser/<token>')
+def read_user(token):
+    database_helper.check_if_logged_in(token)
+
+
+
 #Local functions
 def verify_password(email, password):
     hash = hash_pwd(password)
-    if hash == get_password(email):
+    if hash == database_helper.get_password(email):
         return True
     else:
         return False
@@ -82,7 +100,8 @@ def hash_pwd(password):
 
 @app.teardown_appcontext
 def teardown_app(exception):
-    #Close after app context is teared down.
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 
 if __name__ == '__main__':
