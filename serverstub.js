5e79a35a915188647d87aa4fd29e6a103d70cacf
Andreas Algovik <algovik@gmail.com>
2014-03-05 16:55:27 +0100

    changed serverstub functions to server calls

---
+++ serverstub.js
@@ -0,0 +1,178 @@
+/**
+ * Created by TDDD24.
+ */
+
+var serverstub = new Object();
+var users = null;
+var loggedInUsers = null;
+
+if (localStorage.getItem("users") == null) {
+    users = {};
+}else users = JSON.parse(localStorage.getItem("users"));
+
+if (localStorage.getItem("loggedinusers") == null) {
+    loggedInUsers = {};
+}else loggedInUsers = JSON.parse(localStorage.getItem("loggedinusers"));
+
+// local methods
+serverstub.persistUsers = function(){
+    localStorage.setItem("users", JSON.stringify(users));
+};
+serverstub.persistLoggedInUsers = function(){
+    localStorage.setItem("loggedinusers", JSON.stringify(loggedInUsers));
+};
+serverstub.tokenToEmail = function(token){
+    return loggedInUsers[token];
+};
+serverstub.copyUser = function(user){
+    return JSON.parse(JSON.stringify(user));
+};
+
+// Public methods
+serverstub.signIn = function(email, password){
+    
+    var con = new XMLHttpRequest();
+    var repsonse;
+
+    con.onreadystatechange=function(event){
+        if (event.readyState==4 && event.status==200){
+            response = JSON.parse(event.target.responseText);
+        }
+    }
+
+    con.open("POST", "/signin", true);
+    con.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    con.send("email=" + email + "&password=" + password);
+
+    return response;
+};
+
+serverstub.postMessage = function(token, content, toEmail){
+      
+    var con = new XMLHttpRequest();
+    var repsonse;
+
+    con.onreadystatechange=function(event){
+        if (event.readyState==4 && event.status==200){
+            response = JSON.parse(event.target.responseText);
+        }
+    }
+
+    con.open("POST", "/postmessage", true);
+    con.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    con.send("token=" + token + "&message=" + content + "&email=" + toEmail);
+
+    return response;
+};
+
+/*
+Returns an object containing the field email, firstname, 
+familyname, gender, city and country. 
+*/
+serverstub.getUserDataByToken = function(token){
+    
+    var con = new XMLHttpRequest();
+    var response;
+
+    con.onreadystatechange=function(event){
+        if (event.readyState==4 && event.status==200){
+            response = JSON.parse(event.target.responseText);
+        }
+    }
+
+    con.open("GET", "/getuserdatabytoken?token=" + token, true);
+    con.send(null);
+
+    return response;
+};
+
+serverstub.getUserDataByEmail = function(token, email){
+    
+    var con = new XMLHttpRequest();
+    var response;
+
+    con.onreadystatechange=function(event){
+        if (event.readyState==4 && event.status==200){
+            response = JSON.parse(event.target.responseText);
+        }
+    }
+
+    con.open("GET", "/getuserdatabyemail?token=" + token + "&email=" + email, true);
+    con.send(null);
+
+    return response;
+};
+serverstub.getUserMessagesByToken = function(token){
+    var email = serverstub.tokenToEmail(token);
+    return serverstub.getUserMessagesByEmail(token,email);
+};
+serverstub.getUserMessagesByEmail = function(token, email){
+    if (loggedInUsers[token] != null){
+        if (users[email] != null) {
+            var match = serverstub.copyUser(users[email]).messages;
+            return {"success": true, "message": "User messages retrieved.", "data": match};
+        } else {
+            return {"success": false, "message": "No such user."};
+        }
+    } else {
+        return {"success": false, "message": "You are not signed in."};
+    }
+};
+serverstub.signOut = function(token){
+    
+    var con = new XMLHttpRequest();
+    var response;
+
+    con.onreadystatechange=function(event){
+        if (event.readyState==4 && event.status==200){
+            response = JSON.parse(event.target.responseText);
+        }
+    }
+
+    con.open("GET", "/signout?token=" + token, true);
+    con.send(null);
+
+    return response;
+};
+
+serverstub.signUp = function(formData){ // {email, password, firstname, familyname, gender, city, country}
+    
+    var con = new XMLHttpRequest();
+    var repsonse;
+
+    con.onreadystatechange=function(event){
+        if (event.readyState==4 && event.status==200){
+            response = JSON.parse(event.target.responseText);
+        }
+    }
+
+    con.open("POST", "/signup", true);
+    con.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    con.send("email=" + formData.email +
+            "&password=" + formData.password +
+            "&firstname=" + formData.firstname +
+            "&familyname=" + formData.familyname +
+            "&gender=" + formData.gender +
+            "&city=" + formData.city +
+            "&country=" + formData.country);
+
+    return response;
+};
+
+serverstub.changePassword = function(token, oldPassword, newPassword){
+    
+    var con = new XMLHttpRequest();
+    var repsonse;
+
+    con.onreadystatechange=function(event){
+        if (event.readyState==4 && event.status==200){
+            response = JSON.parse(event.target.responseText);
+        }
+    }
+
+    con.open("POST", "/changepassword?token=" + token, true);
+    con.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    con.send("old_password=" + oldPassword + "&new_password=" + newPassword);
+
+    return response;
+};
